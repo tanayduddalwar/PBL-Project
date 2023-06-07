c7f16f38fa6c246c75a2b536ad91bb632343bac7
vedant-rgb <125129485+vedant-rgb@users.noreply.github.com>
2023-06-07 22:01:07 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,168 +1,153 @@
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    ));
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  )
+);
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var Coloum;
     return Scaffold(
-        body: Container(
-      padding: EdgeInsets.symmetric(vertical: 30),
-      width: double.infinity,
-      decoration: BoxDecoration(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
           gradient: LinearGradient(
-              begin: Alignment.topCenter, colors: [Colors.lightBlue])),
-      child: Coloum(
-          CrossAxisAlignment: CrossAxisAlignment.start,
+            begin: Alignment.topCenter,
+            colors: [
+              Colors.orange,
+              Colors.orange,
+              Colors.orange
+            ]
+          )
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            SizedBox(
-              height: 80,
-            ),
+            SizedBox(height: 80,),
             Padding(
               padding: EdgeInsets.all(20),
-              child: Coloum(
-                CrossAxisAlignment: CrossAxisAlignment.start,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  
-                  Text(
-                    "Login",
-                    style: TextStyle(color: Colors.white, fontSize: 40),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "Welcome Back",
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  )
+                   Text("Login", style: TextStyle(color: Colors.white, fontSize: 40),),
+                  SizedBox(height: 10,),
+                  ( Text("Welcome Back", style: TextStyle(color: Colors.white, fontSize: 18),)),
                 ],
               ),
             ),
-            SizedBox(
-              height: 20,
-            ),
+            SizedBox(height: 20),
             Expanded(
               child: Container(
                 decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(60),
-                        topRight: Radius.circular(60))),
-                child: Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Coloum(children: <Widget>[
-                    Container(
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: [
-                            BoxShadow(
-                                color: Color.fromRGBO(225, 95, 27, .3),
-                                blurRadius: 20,
-                                offset: Offset(0, 10))
-                          ]),
-                      child: Coloum(children: <Widget>[
-                        Container(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(60), topRight: Radius.circular(60))
+                ),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.all(30),
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 60,),
+                         Container(
                           decoration: BoxDecoration(
-                              border: Border(
-                                  bottom: BorderSide(color: Colors.grey))
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [BoxShadow(
+                              color: Color.fromRGBO(225, 95, 27, .3),
+                              blurRadius: 20,
+                              offset: Offset(0, 10)
+                            )]
+                          ),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  border: Border(bottom: BorderSide(color: Colors.grey))
+                                ),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    hintText: "Email or Phone number",
+                                    hintStyle: TextStyle(color: Colors.grey),
+                                    border: InputBorder.none
                                   ),
-                                  child: Coloum(
-                                    children:<Widget>[
-                                      Container(
-                                        decoration: BoxDecoration(
-                                          border: Border(bottom: BorderSide(color:Colors.grey))
-                                        ),
-                                        child: TextField(
-                                          decoration: InputDecoration(
-                                            hintText: "PICT REG ID NO.",
-                                            hintStyle: TextStyle(color:Colors.grey),
-                                            border: InputBorder.none
-                                          ),
-                                        ),
-                                      ),
-                                      Container(
-                                        padding: EdgeInsets.all(10),
-                                        decoration: BoxDecoration(
-                                          border: Border(bottom: BorderSide(color: Colors.grey))
-                                        ),
-                                        child: TextField(
-                                          decoration: InputDecoration(
-                                            hintText: "Password",
-                                            hintStyle: TextStyle(color: Colors.grey),
-                                            border: InputBorder.none
-                                          ),
-                                        ),
-                                      )
-                                    ]
-                      ),
-              
-                        ),
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  border: Border(bottom: BorderSide(color: Colors.grey))
+                                ),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    hintText: "Password",
+                                    hintStyle: TextStyle(color: Colors.grey),
+                                    border: InputBorder.none
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )),
                         SizedBox(height: 40,),
-                        Text("Forgot Password Click Here!",style: TextStyle(color:Colors.grey),),
+                        ( Text("Forgot Password?", style: TextStyle(color: Colors.grey),)),
                         SizedBox(height: 40,),
-                        Container(
+                        ( Container(
                           height: 50,
                           margin: EdgeInsets.symmetric(horizontal: 50),
-                          decoration:BoxDecoration(
+                          decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(50),
                             color: Colors.orange
-
-                          ) ,
-                          child: Center(
-                            child: Text("Login",style: TextStyle(color: Colors.white,fontSize: 16,fontWeight:FontWeight.bold),),
-              
                           ),
-
-
-                        ),
+                          child: Center(
+                            child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                          ),
+                        )),
                         SizedBox(height: 50,),
-                        Text("Continue with social media",style: TextStyle(color: Colors.grey),),
+                        ( Text("Continue with social media", style: TextStyle(color: Colors.grey),)),
                         SizedBox(height: 30,),
                         Row(
                           children: <Widget>[
                             Expanded(
-                              child:Container(
+                              child:( Container(
                                 height: 50,
                                 decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(50),
                                   color: Colors.blue
                                 ),
                                 child: Center(
-                                  child: Text("Facebook",style: TextStyle(color: Colors.white,fontSize: 16,fontWeight:FontWeight.bold),),
-              
-                          ),
-                              ) ,
+                                  child: Text("Facebook", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                                ),
+                              )),
                             ),
-                            SizedBox(width:30,),
+                            SizedBox(width: 30,),
                             Expanded(
-                              child:Container(
+                              child: ( Container(
                                 height: 50,
                                 decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(50),
                                   color: Colors.black
-                                  
                                 ),
                                 child: Center(
-                                  child: Text("Github",style: TextStyle(color: Colors.white,fontSize: 16,fontWeight:FontWeight.bold),),
-              
-                          ),
-                              ),
+                                  child: Text("Github", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                                ),
+                              )),
                             )
                           ],
                         )
-                      ])
-                      ,
-                    )
-                  ]),
+                      ],
+                    ),
+                  ),
                 ),
               ),
             )
-          ]),
-    ));
+          ],
+        ),
+      ),
+    );
   }
 }
