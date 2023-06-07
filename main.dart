37278fc5187da53e305627b2a882697a525d95aa
tanayduddalwar <119003967+tanayduddalwar@users.noreply.github.com>
2023-06-07 19:50:25 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,115 +1,168 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var Coloum;
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: Container(
+      padding: EdgeInsets.symmetric(vertical: 30),
+      width: double.infinity,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter, colors: [Colors.lightBlue])),
+      child: Coloum(
+          CrossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            SizedBox(
+              height: 80,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Coloum(
+                CrossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  
+                  Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Welcome Back",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  )
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(60),
+                        topRight: Radius.circular(60))),
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Coloum(children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Color.fromRGBO(225, 95, 27, .3),
+                                blurRadius: 20,
+                                offset: Offset(0, 10))
+                          ]),
+                      child: Coloum(children: <Widget>[
+                        Container(
+                          decoration: BoxDecoration(
+                              border: Border(
+                                  bottom: BorderSide(color: Colors.grey))
+                                  ),
+                                  child: Coloum(
+                                    children:<Widget>[
+                                      Container(
+                                        decoration: BoxDecoration(
+                                          border: Border(bottom: BorderSide(color:Colors.grey))
+                                        ),
+                                        child: TextField(
+                                          decoration: InputDecoration(
+                                            hintText: "PICT REG ID NO.",
+                                            hintStyle: TextStyle(color:Colors.grey),
+                                            border: InputBorder.none
+                                          ),
+                                        ),
+                                      ),
+                                      Container(
+                                        padding: EdgeInsets.all(10),
+                                        decoration: BoxDecoration(
+                                          border: Border(bottom: BorderSide(color: Colors.grey))
+                                        ),
+                                        child: TextField(
+                                          decoration: InputDecoration(
+                                            hintText: "Password",
+                                            hintStyle: TextStyle(color: Colors.grey),
+                                            border: InputBorder.none
+                                          ),
+                                        ),
+                                      )
+                                    ]
+                      ),
+              
+                        ),
+                        SizedBox(height: 40,),
+                        Text("Forgot Password Click Here!",style: TextStyle(color:Colors.grey),),
+                        SizedBox(height: 40,),
+                        Container(
+                          height: 50,
+                          margin: EdgeInsets.symmetric(horizontal: 50),
+                          decoration:BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Colors.orange
+
+                          ) ,
+                          child: Center(
+                            child: Text("Login",style: TextStyle(color: Colors.white,fontSize: 16,fontWeight:FontWeight.bold),),
+              
+                          ),
+
+
+                        ),
+                        SizedBox(height: 50,),
+                        Text("Continue with social media",style: TextStyle(color: Colors.grey),),
+                        SizedBox(height: 30,),
+                        Row(
+                          children: <Widget>[
+                            Expanded(
+                              child:Container(
+                                height: 50,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50),
+                                  color: Colors.blue
+                                ),
+                                child: Center(
+                                  child: Text("Facebook",style: TextStyle(color: Colors.white,fontSize: 16,fontWeight:FontWeight.bold),),
+              
+                          ),
+                              ) ,
+                            ),
+                            SizedBox(width:30,),
+                            Expanded(
+                              child:Container(
+                                height: 50,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50),
+                                  color: Colors.black
+                                  
+                                ),
+                                child: Center(
+                                  child: Text("Github",style: TextStyle(color: Colors.white,fontSize: 16,fontWeight:FontWeight.bold),),
+              
+                          ),
+                              ),
+                            )
+                          ],
+                        )
+                      ])
+                      ,
+                    )
+                  ]),
+                ),
+              ),
+            )
+          ]),
+    ));
   }
 }
