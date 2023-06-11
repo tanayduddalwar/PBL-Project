cd53a416cf1d96f0d24e4332f10e53a002acddc1
vedant-rgb <125129485+vedant-rgb@users.noreply.github.com>
2023-06-11 13:58:11 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:new_project/IntroPage.dart';
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Splashscreen(),
