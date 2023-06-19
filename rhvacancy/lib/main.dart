cf7e113bec83682a2b4334999c5be8f3a5492555
tanayduddalwar <119003967+tanayduddalwar@users.noreply.github.com>
2023-06-19 16:22:27 +0530

    Update main.dart

--- rhvacancy/lib/main.dart
+++ rhvacancy/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<Users?>.value(
+    return StreamProvider<UserModel?>.value(
       initialData: null,
       value: AuthService().user,
       child: MaterialApp(
