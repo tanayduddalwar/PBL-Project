9ef0c3939a619fdbb7dc0c5861abcf07a66d39d5
vedant-rgb <125129485+vedant-rgb@users.noreply.github.com>
2023-06-22 00:19:58 +0530

    Update vacancy.dart

--- rhvacancy/lib/screens/vacancy.dart
+++ rhvacancy/lib/screens/vacancy.dart
@@ -1,68 +1,212 @@
-import 'package:flutter/material.dart';
-import 'package:rhvacancy/screens/authenticate/sign_in.dart';
+import 'dart:async';
 import 'dart:math';
-import 'package:rhvacancy/services/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+class SeatBookingLayout extends StatelessWidget {
+  final int randomSeats;
 
-class RandomNumberPage extends StatefulWidget {
+  SeatBookingLayout({required this.randomSeats});
+
   @override
-  _RandomNumberPageState createState() => _RandomNumberPageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Vacancy Position'),
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 10, // Number of columns in the grid
+        ),
+        itemCount: 100, // Total number of seats in the grid
+        itemBuilder: (context, index) {
+          // Determine if the seat should be locked or not based on the random number
+          bool isLocked = index < randomSeats;
+
+          return Container(
+            margin: EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              color: isLocked ? Colors.red : Colors.green,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Center(
+              child: Text(
+                'Seat ${index + 1}',
+                style: TextStyle(
+                  color: isLocked ? Colors.white : Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
 }
 
-class _RandomNumberPageState extends State<RandomNumberPage> {
-  Random _random = Random();
-  int _minRange = 1;
-  int _maxRange = 100;
-  late int _randomNumber;
+class RandomNumberScreen extends StatefulWidget {
+  @override
+  _RandomNumberScreenState createState() => _RandomNumberScreenState();
+}
 
-  void generateRandomNumber() {
-    setState(() {
-      _randomNumber = _random.nextInt(_maxRange - _minRange + 1) + _minRange;
-    });
-  }
+class _RandomNumberScreenState extends State<RandomNumberScreen> {
+  FlutterLocalNotificationsPlugin? flutterLocalNotificationsPlugin;
+  Timer? _timer;
+  int randomNum = 0;
+  SharedPreferences? _prefs;
 
   @override
   void initState() {
-    generateRandomNumber();
     super.initState();
+    _initializeFlutterLocalNotifications();
+    _initializeRandomNumber();
+    _startTimer();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  Future<void> _initializeFlutterLocalNotifications() async {
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+    final InitializationSettings initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+    await flutterLocalNotificationsPlugin!.initialize(initializationSettings);
+  }
+
+  Future<void> _showNotification() async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+      'random_number_channel',
+      'Random Number Channel',
+      importance: Importance.defaultImportance,
+      priority: Priority.defaultPriority,
+      showWhen: false,
+    );
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin!.show(
+      0,
+      'Random Number Update',
+      'The random number has been updated.',
+      platformChannelSpecifics,
+    );
+  }
+
+  Future<void> _initializeRandomNumber() async {
+    _prefs = await SharedPreferences.getInstance();
+    final storedRandomNum = _prefs?.getInt('randomNum');
+    final storedTimestamp = _prefs?.getInt('timestamp');
+
+    if (storedRandomNum != null && storedTimestamp != null) {
+      final currentTime = DateTime.now().millisecondsSinceEpoch;
+      final elapsedTime = currentTime - storedTimestamp;
+
+      if (elapsedTime < 2 * 60 * 1000) {
+        // Random number is still valid within the 5-minute timeframe
+        setState(() {
+          randomNum = storedRandomNum;
+        });
+        return;
+      }
+    }
+
+    _updateRandomNumber();
+  }
+
+  void _updateRandomNumber() {
+    final newRandomNum =
+        Random().nextInt(100) + 1; // Generates a random number between 1 and 10
+    _prefs?.setInt('randomNum', newRandomNum);
+    _prefs?.setInt('timestamp', DateTime.now().millisecondsSinceEpoch);
+
+    setState(() {
+      randomNum = newRandomNum;
+    });
+
+    _showNotification();
+  }
+
+  void _startTimer() {
+    _timer = Timer.periodic(Duration(minutes: 2), (_) => _updateRandomNumber());
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Vacancy Position'),
-        backgroundColor: Colors.deepPurple[300],
-        actions: <Widget>[
-          ElevatedButton.icon(
-            icon: Icon(Icons.person),
-            label: Text("Logout"),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) =>SignIn()),
-              );
-            },
-          ),
-        ],
+        backgroundColor: Colors.deepPurpleAccent,
         centerTitle: true,
+        title: Text(
+          'Vacancy Position',
+        ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Random Number:',
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 16),
-            Text(
-              '$_randomNumber',
-              style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/pict_library.jpeg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Total number of seats in reading hall: 100\n",
+                style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    backgroundColor: Colors.grey,
+                    color: Colors.black),
+              ),
+              Text(
+                'Number of occupied seats: $randomNum',
+                style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    backgroundColor: Colors.grey,
+                    color: Colors.black),
+              ),
+              SizedBox(height: 23),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            SeatBookingLayout(randomSeats: randomNum),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.deepPurpleAccent),
+                  child: Text(
+                    'Go to Seat Locking',
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: RandomNumberScreen(),
+  ));
 }
